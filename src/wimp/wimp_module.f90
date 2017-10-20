bb16542ef15fd82fc965005f83230aae6f26743f
troyraen <troyraen@users.noreply.github.com>
2017-10-19 21:29:35 -0400

    Update wimp_module.f90

--- src/wimp/wimp_module.f90
+++ src/wimp/wimp_module.f90
@@ -329,7 +329,7 @@
 		IMPLICIT NONE
 		INTEGER, INTENT(IN) :: id
 		INTEGER, INTENT(OUT) :: ierr
-		INTEGER, idx, j
+		INTEGER :: idx, j
 		TYPE (star_info), pointer :: s ! pointer to star type
 		ierr=0
 		CALL GET_STAR_PTR(id, s, ierr)
