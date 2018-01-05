39fc08d2b751f2713aa62f876e726412a8b05117
troyraen <troy.raen@pitt.edu>
2018-01-05 15:35:05 -0500

    iso

---
+++ isochrones/rnIsoMass_template.sh
@@ -0,0 +1,38 @@
+##!/usr/local/bin/bash
+#!/bin/bash
+
+
+export MESA_DIR=/home/tjr63/mesa-r9793
+export OMP_NUM_THREADS=1
+export MESA_BASE=/home/tjr63/mesa_wimps
+export MESA_INLIST=$MESA_BASE/inlist
+#export MESA_RUN=/home/tjr63/isoc
+export MESA_RUN=/home/tjr63/sand
+
+declare -A svals=( [SD]=.TRUE. [SI]=.FALSE. )
+declare -a sord=(SD SI)
+declare -A cbvals=( [c0]=0.D0 [c1]=1.D1 [c2]=1.D2 [c3]=1.D3 [c4]=1.D4 [c5]=1.D5 )
+declare -a cord=( c0 c1 c2 c3 )
+#declare -A cbvals1=( [c4]=1.D4 ) # run these later
+#declare -A cbvals2=( [c5]=1.D5 )
+declare -A mvals=( [m4p9]=4.9D0 [m3p9]=3.9D0 [m2p9]=2.9D0 [m1p9]=1.9D0 )
+declare -a mord=( m4p9 m3p9 m2p9 m1p9 )
+
+
+for spin in "${sord[@]}"; do
+    for cdir in "${cord[@]}"; do
+        for mass in "${mord[@]}"; do
+            mkdir -p $MESA_RUN/$spin/$cdir/$mass
+                cp $MESA_RUN/xinlist_template $MESA_RUN/$spin/$cdir/$mass/inlist_cluster
+                cd $MESA_RUN/$spin/$cdir/$mass
+                    sed -i 's/s_c_m_/'$spin$cdir$mass'/g; s/cboost_/'${cbvals[$cdir]}'/g; s/imass_/'${mvals[$mass]}'/g; s/SD_/'${svals[$spin]}'/g' inlist_cluster
+#                sed -i 's cboost_ '${cbvals[${cdir}]}' g; s imass_ '${mvals[${mass}]}' g; s SD_ '${svals[$spin]}' g' inlist_cluster
+
+                   $MESA_BASE/star
+
+                cd ..
+                cd ..
+                cd ..
+        done
+    done
+done
--- src/run_star_extras.f
+++ src/run_star_extras.f
@@ -256,16 +256,16 @@
 
 
          ! STOPPING CONDITION:
-          IF ((s% star_age .GT. 1.D8) .AND. (s% time_step .LT. 300.D0)) THEN   ! STOPPING CONDITION
+          IF ((s% star_age .GT. 1.D8) .AND. (s% time_step .LT. 10.D0)) THEN   ! STOPPING CONDITION
             num_dt_low = num_dt_low+1
           ELSE
             num_dt_low = 0
           ENDIF
 
-          IF (num_dt_low .GT. 5000) THEN
+          IF (num_dt_low .GT. 1.0D5) THEN
               extras_finish_step = terminate
               s% termination_code = t_xtra1
-              termination_code_str(t_xtra1) = 'dt less than 300 yrs for more than 5000 steps'
+              termination_code_str(t_xtra1) = 'dt less than 10 yrs for more than 1.0D5 steps'
               OPEN(UNIT=10, FILE='README.md', status='old', action='write', position='append')
               WRITE(10,*) 's% termination_code: ', s% termination_code, &
                         ' term code str: ', termination_code_str(s% termination_code)
