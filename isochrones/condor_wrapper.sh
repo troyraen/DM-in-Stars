0578886f5eaf46dbf258cb87b583d1ff57f02f9f
troyraen <troy.raen@pitt.edu>
2018-01-23 14:16:57 -0500

    run organization

--- isochrones/condor_wrapper.sh
+++ isochrones/condor_wrapper.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-# Use to run all 10 rnIsoMass#.sh files through Condor
-#/home/tjr63/mesa_wimps_4isoc/isochrones/rnIsoMasses/rnIsoMass$1.sh
-
-# Use to make directories:
-for i in {0..9}; do
-        /home/tjr63/mesa_wimps_4isoc/isochrones/rnIsoMasses/rnIsoMass$i.sh
-done
+/home/tjr63/mesa_wimps_4isoc/isochrones/rnIsoMasses/rnIsoMass$1.sh
