952b3302a0d80724366cab28fa2ee16eddbfa307
troyraen <troy.raen@pitt.edu>
2018-05-12 10:18:36 -0400

    condor test with mord=( m4p5 m3p5 m2p5 m1p5 m0p8 )

--- implicit_test_files/rnMESA2.sh
+++ implicit_test_files/rnMESA2.sh
@@ -22,8 +22,8 @@ declare -a sord=( SD )
 declare -A cbvals=( [c0]=0.D0 [c1]=1.D1 [c2]=1.D2 [c3]=1.D3 [c4]=1.D4 [c5]=1.D5 [c6]=1.D6 )
 declare -a cord=( c0 )
 # run the rest of cbvals later
-declare -A mvals=( [m1p0]=1.0D0 [m1p1]=1.1D0 )
-declare -a mord=( m1p1 )
+declare -A mvals=( [m1p5]=1.5D0 [m2p5]=2.5D0 [m3p5]=3.5D0 [m4p5]=4.5D0 [m0p8]=0.8D0 )
+declare -a mord=( m4p5 m3p5 m2p5 m1p5 m0p8 )
 
 
 for spin in "${sord[@]}"; do
