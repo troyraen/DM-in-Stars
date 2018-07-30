2154b1c81108bc2b8375779691ee01e1e6cc28cd
troyraen <troy.raen@pitt.edu>
2018-07-30 11:33:21 -0400

    finish and low mass runs

--- implicit_test_files/rnMESA3.sh
+++ implicit_test_files/rnMESA3.sh
@@ -11,7 +11,7 @@ function check_okay {
 
 export MESA_DIR=/home/tjr63/mesa-r9793
 export OMP_NUM_THREADS=1
-export MESA_BASE=/home/tjr63/implicit_test2i
+export MESA_BASE=/home/tjr63/implicit_test2ii
 # !!! If you change MESA_BASE you must change the file paths in inlist and condor_wrapper !!!
 export MESA_INLIST=$MESA_BASE/inlist
 export MESA_RUN=$MESA_BASE/RUNS
