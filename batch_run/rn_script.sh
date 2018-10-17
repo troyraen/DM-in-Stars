6118fc74f241aceae860f90f4cbcbb1cbd31730b
troyraen <troy.raen@pitt.edu>
2018-10-17 10:36:53 -0400

    batch run wimp_module linear root approx and emom normalized variations

--- batch_run/rn_script.sh
+++ batch_run/rn_script.sh
@@ -1,28 +1,46 @@
 #!/bin/bash
 
 # declare -A ivals=( [mlt]=mlt [sc]=semiconvection [th]=thermohaline [jina]=jina [mts]=mesh_timestep [opa]=opacity [ovs]=overshoot [ow]=other_wind [opos]=opacity_overshoot [oth]=other [OG]=OG)
-declare -A ivals=( [mlt]=mlt [sc]=semiconvection [th]=thermohaline [opa]=opacity [oth]=other [OG]=OG [pre]=preMIST [lim]=limits [hms]=hook_preMST [otm]=op_thrm_mlt)
+# declare -A ivals=( [mlt]=mlt [sc]=semiconvection [th]=thermohaline [opa]=opacity [oth]=other [OG]=OG [pre]=preMIST [lim]=limits [hms]=hook_preMST [otm]=op_thrm_mlt)
 # declare -a iord=( pre opa mlt sc th lim hms oth)
-declare -a iord=(OG pre otm)
+# declare -a iord=(OG pre otm)
+
+declare -A ivals=( [base]=base [enlr]=en_lr [enom]=enom [lr]=lr )
+declare -a iord=(base enlr enom lr)
 
 maindir=/home/tjr63/mesaruns
 outfl=$maindir/LOGS/STD.out
+RUNS=RUNS_xLdivLnuc_test
+xphoto=x773_xLdivLnuc_GT_0p085_photo
+
 for inlst in "${iord[@]}"; do
     mkdir $maindir/LOGS $maindir/png $maindir/photos
-    cp $maindir/inlist_${ivals[$inlst]} $maindir/inlist
-    cp $maindir/inlist $maindir/LOGS/inlist_${ivals[$inlst]}
-    $maindir/rn &> $outfl
+
+    # CHANGE INLIST
+    # cp $maindir/inlist_${ivals[$inlst]} $maindir/inlist
+    # cp $maindir/inlist $maindir/LOGS/inlist_${ivals[$inlst]}
+    # $maindir/rn &> $outfl
+
+    # CHANGE WIMP_MODULE.f90
+    cp $maindir/src/wimp/wimp_module_$inlst.f90 $maindir/src/wimp/wimp_module.f90
+    $maindir/clean
+    $maindir/mk
+
+    cp $maindir/$RUNS/$xphoto $maindir/photos/.
+    $maindir/re $xphoto &>> LOGS/STD.out
+
     $maindir/bash_scripts/del_dup_mods.sh $maindir &>> $outfl
-    $maindir/bash_scripts/data_reduc.sh $maindir &>> $outfl
+    # $maindir/bash_scripts/data_reduc.sh $maindir &>> $outfl
 
     $maindir/pgstar_movie grid1
     mv $maindir/movie.mp4 $maindir/LOGS/grid1.mp4
     $maindir/pgstar_movie grid2
     mv $maindir/movie.mp4 $maindir/LOGS/grid2.mp4
+    rm -r $maindir/png
 
-    newlogs=$maindir/RUNS_emom_normalized/LOGSc5_${ivals[$inlst]}
+    newlogs=$maindir/$RUNS/LOGSc5_${ivals[$inlst]}
     mv $maindir/LOGS $newlogs
     mv $maindir/photos $newlogs/.
-    mv $maindir/png $newlogs/.
+    # mv $maindir/png $newlogs/.
 
 done
