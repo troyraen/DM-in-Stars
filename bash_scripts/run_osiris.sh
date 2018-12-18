c0ac6422321c298ea65f7bc8fe92960aa27b4158
troyraen <troy.raen@pitt.edu>
2018-12-18 16:09:42 -0500

    run_osiris

--- bash_scripts/run_osiris.sh
+++ bash_scripts/run_osiris.sh
@@ -90,19 +90,23 @@ function rnmesa () {
 
 ### MAIN PROGRAM
 ### runs mesa models with specified params
+mkcln="${1:-1}" #  = 1 will execute ./mk and ./clean
+
 maindir="/home/tjr63/mesaruns"
 RUNS="RUNS_pgstarGrid2"
 
 cd ${maindir}
-./clean
-./mk
+if [ "${mkcln}" = 1 ]; then
+        ./clean
+        ./mk
+fi
 
 
-declare -A mvals=( [m0p8]=0.8D0 [m1p0]=1.0D0 [m1p2]=1.2D0 [m1p4]=1.4D0 )
-declare -a mord=( m0p8 m1p0 m1p2 m1p4 )
+declare -A mvals=( [m0p8]=0.8D0 [m1p0]=1.0D0 [m1p1]=1.1D0 [m1p2]=1.2D0 [m1p3]=1.3D0 [m1p4]=1.4D0 [m1p6]=1.6D0 [m2p0]=2.0D0 [m3p0]=3.0D0 [m4p0]=4.0D0 )
+declare -a mord=( m0p8 m1p0 m1p1 m1p2 m1p3 m1p4 m1p6 m2p0 m3p0 m4p0 )
 
-for mass in "${mord[@]}"; do
-    for cb in 0 3 6; do
+for cb in 0 3 6; do
+    for mass in "${mord[@]}"; do
         rnmesa "${maindir}" "${RUNS}/c${cb}/${mass}" "${mvals[${mass}]}" "${cb}" 1 "master"
     done
 done
