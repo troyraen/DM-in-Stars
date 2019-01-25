2c9e653c64ee0eeaa67afe98fb1739604f253932
troyraen <troy.raen@pitt.edu>
2019-01-25 00:19:50 -0500

    move function rnmesa to external script do_mesa_run.sh

--- bash_scripts/run_osiris.sh
+++ bash_scripts/run_osiris.sh
@@ -51,57 +51,57 @@
 # }
 
 
-
-function rnmesa () {
-    maindir=$1 # main mesa_wimps dir (e.g. mesaruns)
-    RUN=${maindir}/$2 # specific directory for log files (e.g. RUNS_/SD/C0/m1p4)
-    mass=$3 # floating point number
-    cboost=$4 # = integer 0..6
-    pgstar=${5:-0} # = 1 generates a movie, default 0
-    inlist_master=${6:-"master"} # inlist_$6 will be used as base inlist
-    stop_TAMS=${7:-0} # = 1 stops the run when h1 frac < 1.e-12
-
-    ### PREP
-    echo
-    if [ -d "${RUN}" ]; then
-        if [ -f "${RUN}/LOGS/history.data" ]; then
-            tm=$(date +"%m%d%y_%H%M")
-            mv ${RUN} ${RUN}_ow_${tm}  # move what's already here
-        else
-            rm -r ${RUN} # no previous history.data file exists. delete the folder
-        fi
-    fi
-    mkdir -p ${RUN}/LOGS ${RUN}/png ${RUN}/photos
-    stdout=${RUN}/LOGS/STD.out
-    # cp $maindir/$RUNS/$xphoto $maindir/photos/.
-    write_inlists ${maindir} ${RUN} ${mass} ${cboost} ${pgstar} ${inlist_master} ${stop_TAMS}
-
-    ### RUN
-    echo "Running MESA ..."
-    cd ${RUN}
-    ${maindir}/star &>> ${stdout}
-    # $maindir/re $xphoto &>> LOGS/STD.out
-    ${maindir}/bash_scripts/del_dup_mods.sh ${RUN}/LOGS &>> ${stdout}
-    # ${maindir}/bash_scripts/data_reduc.sh ${RUN}/LOGS &>> ${stdout}
-
-    ### Finish pgstar movies
-    if [ "${pgstar}" = 1 ]; then
-        images_to_movie.sh "png/grid1*.png" ./LOGS/grid1_${mass}c${cb}.mp4 # make movies
-        images_to_movie.sh "png/grid2*.png" ./LOGS/grid2_${mass}c${cb}.mp4
-        if [ -f ./LOGS/grid2_${mass}c${cb}.mp4 ]; then
-            echo "Pgstar movies created." &>> ${stdout}
-            sed -i "/\.png\/png/d" ${stdout} # strip png lines from stdout
-            rm -r ${RUN}/png # del png files
-        else
-            echo "Something went wrong making pgstar movies!" &>> ${stdout}
-        fi
-    fi
-
-    ### CLEANUP
-    cd ${maindir}
-    echo "Finished ${mass}M_sun cb${cboost}"
-    echo
-}
+###... MOVED TO EXTERNAL FILE do_mesa_run.sh ...###
+# function rnmesa () {
+#     maindir=$1 # main mesa_wimps dir (e.g. mesaruns)
+#     RUN=${maindir}/$2 # specific directory for log files (e.g. RUNS_/SD/C0/m1p4)
+#     mass=$3 # floating point number
+#     cboost=$4 # = integer 0..6
+#     pgstar=${5:-0} # = 1 generates a movie, default 0
+#     inlist_master=${6:-"master"} # inlist_$6 will be used as base inlist
+#     stop_TAMS=${7:-0} # = 1 stops the run when h1 frac < 1.e-12
+#
+#     ### PREP
+#     echo
+#     if [ -d "${RUN}" ]; then
+#         if [ -f "${RUN}/LOGS/history.data" ]; then
+#             tm=$(date +"%m%d%y_%H%M")
+#             mv ${RUN} ${RUN}_ow_${tm}  # move what's already here
+#         else
+#             rm -r ${RUN} # no previous history.data file exists. delete the folder
+#         fi
+#     fi
+#     mkdir -p ${RUN}/LOGS ${RUN}/png ${RUN}/photos
+#     stdout=${RUN}/LOGS/STD.out
+#     # cp $maindir/$RUNS/$xphoto $maindir/photos/.
+#     write_inlists ${maindir} ${RUN} ${mass} ${cboost} ${pgstar} ${inlist_master} ${stop_TAMS}
+#
+#     ### RUN
+#     echo "Running MESA ..."
+#     cd ${RUN}
+#     ${maindir}/star &>> ${stdout}
+#     # $maindir/re $xphoto &>> LOGS/STD.out
+#     ${maindir}/bash_scripts/del_dup_mods.sh ${RUN}/LOGS &>> ${stdout}
+#     # ${maindir}/bash_scripts/data_reduc.sh ${RUN}/LOGS &>> ${stdout}
+#
+#     ### Finish pgstar movies
+#     if [ "${pgstar}" = 1 ]; then
+#         images_to_movie.sh "png/grid1*.png" ./LOGS/grid1_${mass}c${cb}.mp4 # make movies
+#         images_to_movie.sh "png/grid2*.png" ./LOGS/grid2_${mass}c${cb}.mp4
+#         if [ -f ./LOGS/grid2_${mass}c${cb}.mp4 ]; then
+#             echo "Pgstar movies created." &>> ${stdout}
+#             sed -i "/\.png\/png/d" ${stdout} # strip png lines from stdout
+#             rm -r ${RUN}/png # del png files
+#         else
+#             echo "Something went wrong making pgstar movies!" &>> ${stdout}
+#         fi
+#     fi
+#
+#     ### CLEANUP
+#     cd ${maindir}
+#     echo "Finished ${mass}M_sun cb${cboost}"
+#     echo
+# }
 
 
 
@@ -122,9 +122,10 @@ fi
 declare -A mvals=( [m0p8]=0.8 [m1p0]=1.0 [m1p3]=1.3 [m2p5]=2.5 [m3p5]=3.5 [m4p5]=4.5 )
 declare -a mord=( m3p5 m1p3 )
 
-source ${maindir}/bash_scripts/write_inlists.sh
+# source ${maindir}/bash_scripts/write_inlists.sh # now sourced in do_mesa_run.sh
+source ${maindir}/bash_scripts/do_mesa_run.sh
 for cb in 0 6; do
     for mass in "${mord[@]}"; do
-        rnmesa "${maindir}" "${RUNS}/c${cb}/${mass}" "${mvals[${mass}]}" "${cb}" 1 "master" 0
+        do_mesa_run "${maindir}" "${RUNS}/c${cb}/${mass}" "${mvals[${mass}]}" "${cb}" 1 "master" 0
     done
 done
