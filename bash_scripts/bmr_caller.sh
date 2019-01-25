81e28450c2efb85a58e0684fb85f5a0b785de56d
troyraen <troy.raen@pitt.edu>
2019-01-25 00:12:57 -0500

    brm_caller fix

--- bash_scripts/bmr_caller.sh
+++ bash_scripts/bmr_caller.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # This function spawns nscreens screen processes
-#   each running a batch of MESA runs using bulk_mesa_run.sh
+#   each running a batch of MESA runs using bulk_mesa_run.sh.
+#   Function is called in last line of file
 #
 # Function arguments:
 #       run_directory and start, increment, stop
@@ -10,54 +11,61 @@
 #       (maindir and scripts_dir are hardcoded below)
 #   and completes a series of mesa runs by calling do_mesa_run.sh.
 #   Assumes masses have precision no greater than 2 decimal places
-
 #
 
 
-RUNS=$1
-# for mass seq (complete mass set to run)
-mstart=$2
-mstop=$3
-minc=$4
-# mstart=5.0
-# mstop=0.8
-# minc=-0.05
-# for cboost seq
-cstart=$5
-cinc=$6
-cstop=$7
-# cstart=3
-# cinc=1
-# cstop=6
-nscreens=${8:-5} # number of screen processes to spawn
+function bmr_caller () {
+    RUNS=$1
+    # for mass seq (complete mass set to run)
+    mstart=$2
+    minc=$3
+    mstop=$4
+    # mstart=5.0
+    # mstop=0.8
+    # minc=-0.05
+    # for cboost seq
+    cstart=$5
+    cinc=$6
+    cstop=$7
+    # cstart=3
+    # cinc=1
+    # cstop=6
+    nscreens=${8:-5} # number of screen processes to spawn
 
 
 
-#  PREP, separate for screens:
-minc_scrn=$(echo ${nscreens} \* ${minc} | bc -l) # minc per screen
-# echo $(seq ${mstart} ${minc} ${mstop})
-mstart_list=($(seq ${mstart} ${minc} ${mstop})) # separate masses
-mstart_list=(${mstart_list[@]:0:${nscreens}})
+    #  PREP, separate for screens:
+    minc_scrn=$(echo ${nscreens} \* ${minc} | bc -l) # minc per screen
+    # echo $(seq ${mstart} ${minc} ${mstop})
+    mstart_list=($(seq ${mstart} ${minc} ${mstop})) # separate masses
+    mstart_list=(${mstart_list[@]:0:${nscreens}})
 
-# Ask user, run make/clean?
-maindir="/home/tjr63/mesaruns"
-echo
-echo "Run clean and make in dir ${maindir}?"
-echo "  0 = no; 1 = yes"
-read mkcln
-if [ "${mkcln}" = 1 ]; then
-    cd ${maindir}
-    ./clean
-    ./mk
-fi
+    # Ask user, run make/clean?
+    maindir="/home/tjr63/mesaruns"
+    echo
+    echo "Run clean and make in dir ${maindir}?"
+    echo "  0 = no; 1 = yes"
+    read mkcln
+    if [ "${mkcln}" = 1 ]; then
+        cd ${maindir}
+        ./clean
+        ./mk
+    fi
 
-scripts_dir="/home/tjr63/mesaruns/bash_scripts"
-source ${scripts_dir}/bulk_mesa_run.sh
-cd ${scripts_dir}
-for ms in "${mstart_list[@]}"; do
-    echo "doing bmr_caller ${ms}" >> ${fout}
-    screen -dm bulk_mesa_run ${RUNS} ${ms} ${minc_scrn} ${mstop} ${cstart} ${cinc} ${cstop}
-	# echo "list $ms"
-    # echo $(seq $ms $minc_scrn $mstop)
-	# echo
-done
+    scripts_dir="/home/tjr63/mesaruns/bash_scripts"
+    source ${scripts_dir}/bulk_mesa_run.sh
+    cd ${scripts_dir}
+    for ms in "${mstart_list[@]}"; do
+        echo
+        echo "bmr_caller calling bulk_mesa_run with start mass: ${ms} Msun."
+        echo "  Runs folder is ${RUNS}."
+        screen -dm bulk_mesa_run ${RUNS} ${ms} ${minc_scrn} ${mstop} ${cstart} ${cinc} ${cstop}
+    	# echo "list $ms"
+        # echo $(seq $ms $minc_scrn $mstop)
+    	# echo
+    done
+
+}
+
+
+bmr_caller "RUNS_test_final" 5.0 -0.05 0.79 3 1 6
