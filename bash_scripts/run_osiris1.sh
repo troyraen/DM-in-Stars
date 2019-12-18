f5f4c5af326e65121bf5cb0df05df778ea0b3487
troyraen <troy.raen@pitt.edu>
2019-12-18 13:08:57 -0500

    do run key: _defDM

--- bash_scripts/run_osiris1.sh
+++ bash_scripts/run_osiris1.sh
@@ -3,7 +3,7 @@
 ### runs mesa models with specified params
 maindir="/home/tjr63/DMS/mesaruns"
 cd ${maindir}
-RUNS="RUNS_3test_final"
+RUNS="RUNS_runSettings"
 
 # Ask user, run make/clean?
 echo
@@ -16,11 +16,12 @@ if [ "${mkcln}" = 1 ]; then
 fi
 
 declare -A mvals=( [m0p8]=0.8 [m1p0]=1.0 [m1p5]=1.5 [m2p0]=2.0 [m2p5]=2.5 [m3p0]=3.0 [m3p5]=3.5 [m4p0]=4.0 [m4p5]=4.5 [m5p0]=5.0 )
-declare -a mord=( m5p0 m4p5 m4p0 m3p5 m3p0 m2p5 m2p0 m1p5 m1p0 m0p8 )
+# declare -a mord=( m5p0 m4p5 m4p0 m3p5 m3p0 m2p5 m2p0 m1p5 m1p0 m0p8 )
+declare -a mord=( m4p5 m3p5 m2p5 m1p0 )
 
 # source ${maindir}/bash_scripts/write_inlists.sh # now sourced in do_mesa_run.sh
 source ${maindir}/bash_scripts/do_mesa_run.sh
-for cb in 0 1 2 3 4 5 6; do
+for cb in 0 6; do
     for mass in "${mord[@]}"; do
         do_mesa_run "${maindir}" "${RUNS}/c${cb}/${mass}" "${mvals[${mass}]}" "${cb}" 0 "master" 0
     done
