0cd96e090f45c649c6e887b6d303211b386f2595
troyraen <troy.raen@pitt.edu>
2019-12-20 18:43:17 -0500

    do run mist0m7

--- bash_scripts/run_osiris1.sh
+++ bash_scripts/run_osiris1.sh
@@ -3,7 +3,7 @@
 ### runs mesa models with specified params
 maindir="/home/tjr63/DMS/mesaruns"
 cd ${maindir}
-RUNS="RUNS_runSettings/mist12"
+RUNS="RUNS_runSettings/mist0m7"
 
 # Ask user, run make/clean?
 echo
