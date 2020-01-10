1bfadcdceb546f677c137d19e5d6781109123cca
troyraen <troy.raen@pitt.edu>
2020-01-10 17:19:36 -0500

    do m1p0

--- bash_scripts/run_osirisRTT.sh
+++ bash_scripts/run_osirisRTT.sh
@@ -10,7 +10,7 @@ RUNS="RUNS_runtimeTests/${run_key}/threads${ont}/sim${sim}"
 
 
 declare -A mvals=( [m0p8]=0.8 [m1p0]=1.0 [m1p5]=1.5 [m2p0]=2.0 [m2p5]=2.5 [m3p0]=3.0 [m3p5]=3.5 [m4p0]=4.0 [m4p5]=4.5 [m5p0]=5.0 )
-declare -a mord=( m0p8 )
+declare -a mord=( m1p0 )
 
 
 source ${maindir}/bash_scripts/do_mesa_run.sh
