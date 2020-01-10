15e224aca67e69cd3422bc37196a038b74eb1918
troyraen <troy.raen@pitt.edu>
2020-01-10 12:05:38 -0500

    adds run_osirisRTT.sh and caller

---
+++ bash_scripts/run_osirisRTT.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+### runs mesa models with specified params
+run_key=$1
+maindir="/home/tjr63/DMS/mesaruns"
+cd ${maindir}
+RUNS="RUNS_runtimeTests/${run_key}"
+
+
+declare -A mvals=( [m0p8]=0.8 [m1p0]=1.0 [m1p5]=1.5 [m2p0]=2.0 [m2p5]=2.5 [m3p0]=3.0 [m3p5]=3.5 [m4p0]=4.0 [m4p5]=4.5 [m5p0]=5.0 )
+declare -a mord=( m0p8 )
+
+
+source ${maindir}/bash_scripts/do_mesa_run.sh
+
+for cb in 6; do
+    for mass in "${mord[@]}"; do
+        do_mesa_run "${maindir}" "${RUNS}/c${cb}/${mass}" "${mvals[${mass}]}" "${cb}" 0 "master" 0
+    done
+done
---
+++ bash_scripts/run_osirisRTT_caller.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+run_key=$1
+ont=$2 # OMP_NUM_THREADS
+
+cp "inlist_master${run_key}" "inlist_master"
+echo "copied inlist_master${run_key} to inlist_master"
+if [[ "${run_key}" =~ ^(_mist01m9|_mist03m9|_mist06m9)$ ]]; then
+    cp "src/run_star_extras${run_key}.f" "src/run_star_extras.f"
+    echo "copied src/run_star_extras${run_key}.f to src/run_star_extras.f"
+else
+    cp "src/run_star_extras_default_plus_DM.f" "src/run_star_extras.f"
+    echo "copied src/run_star_extras_default_plus_DM.f to src/run_star_extras.f"
+fi
+
+./clean
+./mk
+
+
+export OMP_NUM_THREADS=${ont}
+
+nohup nice ./bash_scripts/run_osirisRTT.sh ${run_key} &>> STD1_nohup_RTT.out &
+nohup nice ./bash_scripts/run_osirisRTT.sh ${run_key} &>> STD2_nohup_RTT.out &
+nohup nice ./bash_scripts/run_osirisRTT.sh ${run_key} &>> STD3_nohup_RTT.out &
